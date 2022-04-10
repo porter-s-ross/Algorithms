518ca8c5cfe4ab7bece4c78a1d700914aca64661
opqlns <opqlns@pm.me>
2022-04-09 22:42:14 -0700

    edit

--- basic13.js
+++ basic13.js
@@ -139,7 +139,6 @@ function zeroOutArrayNegativeVals(arr) {
 // Given an array, move all values forward (to the left) by one index, dropping the first value and leaving a 0 (zero) value at the end of the array.
 
 function shiftArrayValsLeft(arr){
-    var temp = 0;
     for(var i=0; i<arr.length; i++){
         arr[i] = arr[i+1]
         console.log(arr)
