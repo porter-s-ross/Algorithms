058a2ffef7f014681f81b7f6292a9b59839938b6
opqlns <opqlns@pm.me>
2022-05-03 11:21:18 -0500

    added "TwelveBarBlues"

---
+++ Fundamentals/Twelve-BarBlues.js
@@ -0,0 +1,10 @@
+// Write a function that console.logs the number 1, then "chick", then "boom", then "chick", then 2, then "chick", "boom", "chick" – continuing the same cycle for each number up to (including) 12.
+
+function twelveBarBlues(){
+    for(var i=1; i<13; i++){
+        console.log(`${i}, chick, boom, chick`)
+    }
+}
+
+let call = twelveBarBlues();
+console.log(call);
